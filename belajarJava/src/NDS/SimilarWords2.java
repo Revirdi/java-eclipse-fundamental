79d63976a5821e72caa5772fbdb50fdd617aaf86
Revirdi <ardwiyanrevirdi99@gmail.com>
2022-12-13 13:19:55 +0700

    Fixing

--- belajarJava/src/NDS/SimilarWords2.java
+++ belajarJava/src/NDS/SimilarWords2.java
@@ -10,24 +10,23 @@ public class SimilarWords2 {
 	public static int similarWords(String[] s) {
 
 		int result = 0;
+		int check = 0;
 
 		for (int i = 0; i < s.length - 1; i++) {
 
-			String currentStr = s[i];
-			String nextStr = s[i + 1];
-			String[] arrayNext = nextStr.split("");
+			String currentStr = s[i]; // ab
 
-			for (int j = i + 1; j < s.length - 1; j++) {
-
-				int check = 0;
+			for (int j = i; j < s.length - 1; j++) {
+				String nextStr = s[j]; // ba
+				String[] arrayNext = nextStr.split(""); // ["b", "a"]
 				for (int k = 0; k < arrayNext.length; k++) {
 					if (!currentStr.contains(arrayNext[k])) {
 						check = 0;
 					}
 					check = 1;
 				}
-				result += check;
 			}
+			result += check;
 
 		}
 
